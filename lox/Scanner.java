0481e11046823646cf7c17f200853d8408c6c370
Thiago Araujo <thd.araujo@gmail.com>
2024-06-05 09:41:16 -0600

    basic structure

---
+++ lox/Scanner.java
@@ -0,0 +1,20 @@
+package lox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private final String source;
+
+    public Scanner(String source){
+        this.source = source;
+    }
+
+    public List<Token> scanTokens(){
+        List<Token> tokens = new ArrayList<>();
+
+        //TODO
+        
+        return tokens;
+    }
+}
---
+++ lox/Token.java
@@ -0,0 +1,5 @@
+package lox;
+
+public class Token {
+    //TODO    
+}
